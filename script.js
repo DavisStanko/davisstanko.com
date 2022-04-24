ede6820b7cb6014dcb1b693d4404b045f33b22f8
davis stanko <davisbvstanko@gmail.com>
2022-04-24 09:39:45 -0400

    dimmer not working

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 function moneropopup() {
   var popup = document.getElementById("moneropopup");
   popup.classList.toggle("show");
+  var popup = document.getElementById("moneropopup");
+  popup.classList.toggle("show");
 }
 
 function bitcoinpopup() {
+    var popup = document.getElementById("bitcoinpopup");
+    popup.classList.toggle("show");
     var popup = document.getElementById("bitcoinpopup");
     popup.classList.toggle("show");
 }
 
   function paypalpopup() {
     var popup = document.getElementById("paypalpopup");
+    popup.classList.toggle("show");
+    var popup = document.getElementById("dimmer");
     popup.classList.toggle("show");
 }
