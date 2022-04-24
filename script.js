88dde28435a7469a8006e04af8c03a3c4e83c0f5
davis stanko <davisbvstanko@gmail.com>
2022-04-23 20:18:43 -0400

    Donation Popup

---
+++ script.js
@@ -0,0 +1,15 @@
+// When the user clicks on <div>, open the popup
+function moneropopup() {
+  var popup = document.getElementById("moneropopup");
+  popup.classList.toggle("show");
+}
+
+function bitcoinpopup() {
+    var popup = document.getElementById("bitcoinpopup");
+    popup.classList.toggle("show");
+}
+
+  function paypalpopup() {
+    var popup = document.getElementById("paypalpopup");
+    popup.classList.toggle("show");
+}
