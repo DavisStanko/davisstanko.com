a86061553f93f182f29985335525d387f50b7031
davis stanko <davisbvstanko@gmail.com>
2022-05-02 18:02:26 -0400

    close popup when dimmer clicked

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// When the user clicks on <div>, open the popup
+// When the user clicks on Monero, open the Monero popup
 function moneropopup() {
   var popup = document.getElementById("moneropopup");
   popup.classList.toggle("show");
@@ -6,6 +6,7 @@ function moneropopup() {
   popup.classList.toggle("show");
 }
 
+// When the user clicks on Bitcoin, open the Bitcoin popup
 function bitcoinpopup() {
     var popup = document.getElementById("bitcoinpopup");
     popup.classList.toggle("show");
@@ -13,9 +14,19 @@ function bitcoinpopup() {
     popup.classList.toggle("show");
 }
 
+//  When the user clicks on PayPal, open the PayPal popup
   function paypalpopup() {
     var popup = document.getElementById("paypalpopup");
     popup.classList.toggle("show");
     var popup = document.getElementById("dimmer");
     popup.classList.toggle("show");
+}
+
+// Close ALL popups if the user clicks outside of it
+function hideall () {
+  var elems = document.querySelectorAll(".show");
+
+  [].forEach.call(elems, function(el) {
+      el.classList.remove("show");
+  });
 }
