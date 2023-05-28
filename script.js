c09d555ce8afcf260918c619095b529fa162cb0a
davis stanko <davisbvstanko@gmail.com>
2023-05-28 15:31:42 -0400

    slideshow

--- script.js
+++ script.js
@@ -24,7 +24,25 @@ dropdowns.forEach(function(dropdown) {
   });
 });
 
+// Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
 
 //POPUP CODE
 // When the user clicks on Monero, open the Monero popup
